bbb448b3399a2d61e37c1b58f680f0e86f5fc7f2
fzninja <497915773@qq.com>
2016-10-15 01:35:34 +0800

    修改个bug
    1

--- js/content.js
+++ js/content.js
@@ -15,31 +15,12 @@
 
 // $("[data-toggle=popover]").popover();
 
-// //折叠ibox
-// $('.collapse-link').click(function () {
-//     var ibox = $(this).closest('div.ibox');
-//     var button = $(this).find('i');
-//     var content = ibox.find('div.ibox-content');
-//     content.slideToggle(200);
-//     button.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
-//     ibox.toggleClass('').toggleClass('border-bottom');
-//     setTimeout(function () {
-//         ibox.resize();
-//         ibox.find('[id^=map-]').resize();
-//     }, 50);
-// });
 
-// //关闭ibox
-// $('.close-link').click(function () {
-//     var content = $(this).closest('div.ibox');
-//     content.remove();
-// });
-
-// //判断当前页面是否在iframe中
-// if (top == this) {
-//     var gohome = '<div class="gohome"><a class="animated bounceInUp" href="index.html?v=4.0" title="返回首页"><i class="fa fa-home"></i></a></div>';
-//     $('body').append(gohome);
-// }
+//判断当前页面是否在iframe中
+if (top == this) {
+    var gohome = '<div class="gohome"><a class="animated bounceInUp" href="index.html?v=4.0" title="返回首页"><i class="fa fa-home"></i></a></div>';
+    $('body').append(gohome);
+}
 
 //animation.css
 function animationHover(element, animation) {
@@ -56,17 +37,17 @@ function animationHover(element, animation) {
         });
 }
 
-// //拖动面板
-// function WinMove() {
-//     var element = "[class*=col]";
-//     var handle = ".ibox-title";
-//     var connect = "[class*=col]";
-//     $(element).sortable({
-//             handle: handle,
-//             connectWith: connect,
-//             tolerance: 'pointer',
-//             forcePlaceholderSize: true,
-//             opacity: 0.8,
-//         })
-//         .disableSelection();
-// };
+//拖动面板
+function WinMove() {
+    var element = "[class*=col]";
+    var handle = ".ibox-title";
+    var connect = "[class*=col]";
+    $(element).sortable({
+            handle: handle,
+            connectWith: connect,
+            tolerance: 'pointer',
+            forcePlaceholderSize: true,
+            opacity: 0.8,
+        })
+        .disableSelection();
+};
